6f384c9424eb76d074c1fbe3d31c37f9f27b520b
wy <wangyuwz@htgd.com.cn>
2021-01-05 18:03:04 +0800

    解决中文乱码问题,添加返回json时候将long类型转换为String类型的转换器

--- src/main/java/com/ly/storeserver/config/WebConfig.java
+++ src/main/java/com/ly/storeserver/config/WebConfig.java
@@ -1,17 +1,27 @@
 package com.ly.storeserver.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.ly.storeserver.handler.DefaultAuthenticationInterceptor;
 import com.ly.storeserver.utils.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @Description:
@@ -55,5 +65,39 @@ public class WebConfig implements WebMvcConfigurer {
         return new DefaultAuthenticationInterceptor(jwtTokenUtil);
     }
 
+    /**
+     * 解决中文乱码问题
+     *
+     * @return
+     */
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(responseBodyConverter());
+    }
+
+    /**
+     * 返回json时候将long类型转换为String类型的转换器
+     *
+     * @return MappingJackson2HttpMessageConverter
+     */
+    @Bean(name = "longToStringConverter")
+    public MappingJackson2HttpMessageConverter longToStringConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        //前端多传参数  后端忽略  不然报错
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //给前端返回值处理 long 转为json  防止精度缺失
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        mapper.registerModule(simpleModule);
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
 
 }
